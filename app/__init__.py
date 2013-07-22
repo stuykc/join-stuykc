c949001f94cc5581073fce0aec8fb343f06e47d8
Danny Qiu <dqiu55@gmail.com>
2013-07-22 03:46:05 -0400

    Add function for submitting form and landing page

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.secret_key = 'stuykc_beaver_one_beaver_all_lets_all_do_the_beaver_call'
 
-from app import views
+from app import views, add
--- app/views.py
+++ app/views.py
@@ -1,6 +1,16 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for, request
 from app import app
+from add import add_member
 
 @app.route('/', methods = ['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        add_member(request.form['name'], request.form['id'], request.form['email'])
+        flash(request.form['email'])
+        return redirect(url_for('join'))
+    else:
+        return render_template('index.html')
+
+@app.route('/join')
 def join():
-    return render_template("join.html")
+    return render_template('join.html')
