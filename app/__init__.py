4b3bd1cfef17ea18fa00f59a9e6eead41a552781
Danny Qiu <dqiu55@gmail.com>
2013-07-24 00:16:25 -0400

    Port code over to Google App Engine

--- app/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-app.secret_key = 'stuykc_beaver_one_beaver_all_lets_all_do_the_beaver_call'
-
-from app import views, add
---
+++ join/views.py
@@ -0,0 +1,37 @@
+import os, webapp2, jinja2
+from add import add_member
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')),
+    extensions=['jinja2.ext.autoescape'])
+
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render({}))
+
+class JoinForm(webapp2.RequestHandler):
+
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('join.html')
+        self.response.write(template.render({}))
+
+    def post(self):
+        name = self.request.get('name')
+        stuyId = self.request.get('id')
+        email = self.request.get('email')
+
+        add_member(name, stuyId, email)
+
+        template_values = {
+            'email': email
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('submit.html')
+        self.response.write(template.render(template_values))
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/join', JoinForm)
+], debug=True)
