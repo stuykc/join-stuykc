23a58bf93c9d665c2ea038a61c1369001f6f3c12
Danny Qiu <dqiu55@gmail.com>
2015-06-24 22:20:16 -0400

    Add email sending functionality on successful submit

--- join/members.py
+++ join/members.py
@@ -1,6 +1,7 @@
 import urllib, urllib2
 import logging
 from google.appengine.ext import ndb
+from google.appengine.api import mail
 from models import *
 
 def add_member(name, stuy_id, email):
@@ -23,3 +24,27 @@ def add_member(name, stuy_id, email):
     else:
         return False
 
+def send_email(name, stuy_id, email):
+    message = mail.EmailMessage(
+            sender = "Stuyvesant Key Club <stuyvesantkeyclub@gmail.com>",
+            subject = "Thank you for joining Stuyvesant Key Club!"
+    )
+    message.to = email
+    message.body = """
+Hey %s,
+
+Thank you for signing up to be a part of Stuyvesant Key Club! We are part of the world's largest service organizations that exist today!
+
+You had submitted the following information:
+    Name: %s
+    4 Digit ID: %s
+    Email: %s
+
+We are very excited to have you join us. We hope you join us on our facebook group (https://www.facebook.com/groups/stuyvesantkeyclub/), where we will post important updates and about other awesome news. We also have a mailing list that you should definitely sign up for(https://groups.google.com/forum/#!forum/stuyvesant-key-club)!
+
+See you at our next meeting/event!
+
+Best,
+The Stuyvesant Key Club Cabinet
+""" % (name, name, stuy_id, email)
+    message.send()
