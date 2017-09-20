859a0bf9e9df2cb2ceb489f42e5c4ba3e768909f
jasonlam00 <jlam6@stuy.edu>
2017-09-19 22:58:29 -0400

    Update submit.py

--- join/submit.py
+++ join/submit.py
@@ -7,6 +7,7 @@ class SubmitHandler(BaseHandler):
         name = self.request.get('name')
         email = self.request.get('email')
         status = self.request.get('status')
+        stuy_id = self.request.get('id')
         osis = self.request.get('osis')
         phone = self.request.get('phone')
         homeroom = self.request.get('homeroom')
